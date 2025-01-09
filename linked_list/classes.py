8d617494bedd543006569bed8537fcf77506db0e
ReinisDelvers <reinis.delvers@gmail.com>
2025-01-09 16:17:36 +0200

    Update classes.py

--- linked_list/classes.py
+++ linked_list/classes.py
@@ -11,21 +11,52 @@ class Node:
 class List:
     def __init__(self):
         self.first = None
+        self.count = 0
     
-    def add(self, new):
-        if self.first == None:
-            self.first = Node(new)
+    def add(self, new, index = -1):
+        if index < 0 or index >= self.count:
+            if self.first == None:
+                self.first = Node(new)
+            else:
+                last = self.first
+                while last.next:
+                    last = last.next
+                last.next = Node(new, before = last)
         else:
-            last = self.first
-            while last.next:
-                last = last.next
-            last.next = Node(new, before = last)
+            if index == 0:
+                element = Node(new, after = self.first)
+                element.next.prev = element
+                self.first = element
+            else:
+                tail = self.first
+                for i in range(index):
+                    tail = tail.next
+                head = tail.prev
+                element = Node(new, head, tail)
+                head.next = element
+                tail.prev = element
+        self.count += 1
+    
+    def pop(self):
+        if self.count == 0:
+            print("Nothing to delete!")
+            return
+        if self.count == 1:
+            self.first = None
+            self.count = 0
+            return
+        before_last = self.first
+        while before_last.next.next:
+            before_last = before_last.next
+        before_last.next = None
+        self.count -= 1
+        return
 
     def read(self):
         if self.first == None:
             print("List is empty!")
 
-        current= self.first
+        current = self.first
         while current:
             current.read()
             current = current.next
@@ -35,9 +66,12 @@ list = List()
 list.add("Suns")
 list.add("Kāja")
 list.add("Māja")
-list.add(2)
+list.add(2,1)
+list.pop()
+list.add("Kaija")
+list.add("Mala")
 
 list.read()
 print("=============")
 
-list.first.next.next.next.prev.prev.prev.read()
+list.first.next.next.next.prev.prev.read()
