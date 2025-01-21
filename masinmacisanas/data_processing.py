a9b7180c03979723df51ac0ce266934e5a464460
ReinisDelvers <reinis.delvers@gmail.com>
2025-01-21 12:29:32 +0200

    .

---
+++ masinmacisanas/data_processing.py
@@ -0,0 +1,20 @@
+import pandas as pd # Failu apstrāde
+import matplotlib.pyplot as plt # vizualizācija grafiki
+import seaborn as sb # vizualizācijas
+
+sb.set_style("whitegrid")
+plt.rcParams["figure.figsize"] = (15,10)
+
+def heat_map(file):
+    data_file = pd.read_csv(file).select_dtypes("number")
+    sb.heatmap(data_file.corr(), annot=True, cmap="magma")
+    plt.show()
+    return
+
+def distribution(file, colomn):
+    data_file = pd.read_csv(file)
+    sb.histplot(data_file[colomn], color="r")
+    plt.show()
+
+
+heat_map("masinmacisanas/auto_imports.csv")
